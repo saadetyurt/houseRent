41057d57af47457ddf9aaf1358e7e8532802505a
saadetyurt <yurtsaadet06@gmail.com>
2020-05-17 06:35:13 +0300

    14. video bitti slider ayarlarını statik olarak çektim ve adminde düzenlemeler yaptım

--- house/admin.py
+++ house/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from house.models import Category, House
+from mptt.admin import MPTTModelAdmin
 
+from house.models import Category, House, Images
+
+class HouseImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status', 'image_tag']
     list_filter = ['status']
+    readonly_fields = ('image_tag',)
+
 class HouseAdmin(admin.ModelAdmin):
-    list_display = ['title','category', 'price', 'room', 'floor', 'm2', 'image', 'status']
+    list_display = ['title','category', 'price', 'room', 'floor', 'm2', 'address', 'image_tag', 'status']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
-admin.site.register(Category, CategoryAdmin)
+    inlines = [HouseImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'house', 'image', 'image_tag']
+    readonly_fields = ('image_tag',)
+
+admin.site.register(Category, MPTTModelAdmin)
 admin.site.register(House, HouseAdmin)
+admin.site.register(Images, ImagesAdmin)
--- house/models.py
+++ house/models.py
@@ -1,41 +1,80 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+
+class Category(MPTTModel):
     STATUS = (
         ('True', 'Evet'),
         ('False' , 'Hayır'),
     )
-    title = models.CharField(max_length=30)
-    keywords = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, upload_to='images/')
+    title = models.CharField(blank=True, max_length=100)
+    keywords = models.CharField(blank=True, max_length=255)
+    description = models.CharField(blank=True, max_length=255)
+    image = models.ImageField(upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = TreeForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_up = models.DateTimeField(auto_now_add=True)
     update_up = models.DateTimeField(auto_now=True)
 
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
     def __str__(self):
-        return self.title
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k =k.parent
+        return ' -> '.join(full_path[::-1])
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
 class House(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False' , 'Hayır'),
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE) #relation with Category table
-    title = models.CharField(max_length=30)
-    keywords = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, upload_to='images/')
-    price = models.FloatField()
-    detail = models.TextField()
-    room = models.IntegerField()
-    floor = models.IntegerField()
-    m2 = models.IntegerField()
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=255)
+    address = models.CharField(blank=True,max_length=255)
+    image = models.ImageField(upload_to='images/')
+    price = models.FloatField(blank=True,null=True)
+    detail = RichTextUploadingField()
+    slug = models.SlugField(blank=True, max_length=150)
+    room = models.IntegerField(blank=True,null=True)
+    floor = models.IntegerField(blank=True,null=True)
+    m2 = models.IntegerField(blank=True,null=True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+class Images(models.Model):
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
